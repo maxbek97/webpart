17adfc35fa52d55a7afe8c29e449e2b87d08b9d3
maxbek97 <maxbek294@gmail.com>
2026-05-29 18:39:13 +0300

    Added new techcards

--- src/pages/mainpage/components/TechSection.tsx
+++ src/pages/mainpage/components/TechSection.tsx
@@ -9,7 +9,10 @@ const TechSection: React.FC = () => {
     <section id="tech" className="tech-section">
       <div className='tech-box'>
         <TechTile iconName='ASP.svg' description="Мощный фреймворк для построения бэкэнда"></TechTile>
-        <TechTile iconName='Ultralytics YOLOv8.svg' description="Данная модель позволь идентифицировать и отслеживать метки с высокой точностью"></TechTile>
+        {/* <TechTile iconName='Ultralytics YOLOv8.svg' description="Данная модель позволь идентифицировать и отслеживать метки с высокой точностью"></TechTile> */}
+        <TechTile iconName='fastapi-logo-png.png' description='Фреймоврк для быстрого создания и настройки WebSocket-сервера'></TechTile>
+        <TechTile iconName='OpenCV.svg' description='Открытая библиотека для использования компьютерного зрения'></TechTile>
+        <TechTile iconName='kotlin-logo.png' description='Актуальный язык для нпаисания приложения для Android'></TechTile>
       </div>
     </section>
   );
