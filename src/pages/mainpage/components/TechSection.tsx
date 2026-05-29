1d6615b83fb86b59966e6e117c563f02be98504c
maxbek97 <maxbek294@gmail.com>
2026-05-29 18:51:56 +0300

    Changed Icons and site name

--- src/pages/mainpage/components/TechSection.tsx
+++ src/pages/mainpage/components/TechSection.tsx
@@ -10,9 +10,9 @@ const TechSection: React.FC = () => {
       <div className='tech-box'>
         <TechTile iconName='ASP.svg' description="Мощный фреймворк для построения бэкэнда"></TechTile>
         {/* <TechTile iconName='Ultralytics YOLOv8.svg' description="Данная модель позволь идентифицировать и отслеживать метки с высокой точностью"></TechTile> */}
-        <TechTile iconName='fastapi-logo-png.png' description='Фреймоврк для быстрого создания и настройки WebSocket-сервера'></TechTile>
+        <TechTile iconName='fastapi-logo-png.png' description='Фреймоворк для быстрого создания и настройки WebSocket-сервера'></TechTile>
         <TechTile iconName='OpenCV.svg' description='Открытая библиотека для использования компьютерного зрения'></TechTile>
-        <TechTile iconName='kotlin-logo.png' description='Актуальный язык для нпаисания приложения для Android'></TechTile>
+        <TechTile iconName='kotlin-logo.png' description='Актуальный язык для написания приложения для Android'></TechTile>
       </div>
     </section>
   );
